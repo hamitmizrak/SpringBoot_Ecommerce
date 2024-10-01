f9c36e99c9227d292a00d3c378f2c81568c4bb5a
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-01 16:38:49 +0300

    CustomerServicesImpl

--- src/main/java/com/hamitmizrak/springboot_ecommerce/business/services/impl/CustomerServicesImpl.java
+++ src/main/java/com/hamitmizrak/springboot_ecommerce/business/services/impl/CustomerServicesImpl.java
@@ -5,11 +5,12 @@ import com.hamitmizrak.springboot_ecommerce.business.dto.CustomerDto;
 import com.hamitmizrak.springboot_ecommerce.business.services.ICustomerServices;
 import com.hamitmizrak.springboot_ecommerce.data.entity.CustomerEntity;
 import com.hamitmizrak.springboot_ecommerce.data.repository.ICustomerRepository;
+import com.hamitmizrak.springboot_ecommerce.exception.HamitMizrakException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.ArrayList;
 import java.util.List;
 
 // LOMBOK
@@ -40,12 +41,12 @@ public class CustomerServicesImpl implements ICustomerServices<CustomerDto, Cust
     @Transactional
     @Override
     public CustomerDto customerServiceCreate(CustomerDto customerDto) {
-        if(customerDto!=null){
-            CustomerEntity customerEntity =dtoToEntity(customerDto);
+        if (customerDto != null) {
+            CustomerEntity customerEntity = dtoToEntity(customerDto);
             iCustomerRepository.save(customerEntity);
             // customerDto.setCustomerId(customerEntity.getCustomerId());
             // customerDto.setCreatedDate(customerEntity.getCreatedDate());
-        }else{
+        } else {
             throw new NullPointerException("CustomerDto is null");
         }
         return customerDto;
@@ -54,26 +55,69 @@ public class CustomerServicesImpl implements ICustomerServices<CustomerDto, Cust
     // LIST
     @Override
     public List<CustomerDto> customerServiceList() {
-        return List.of();
+        Iterable<CustomerEntity> iterableCustomerList = iCustomerRepository.findAll();
+        List<CustomerDto> customerDtoList = new ArrayList<>();
+        for (CustomerEntity temp : iterableCustomerList) {
+            CustomerDto customerDto = entityToDto(temp);
+            customerDtoList.add(customerDto);
+        }
+        log.info("Customer Listesi: " + customerDtoList.size() + " tanedir");
+        return customerDtoList;
     }
 
     // FIND
     @Override
     public CustomerDto customerServiceFindById(Long id) {
-        return null;
+        // FIND 1.YOL
+        /*
+        Optional<CustomerEntity> findOptionalById= iCustomerRepository.findById(id);
+        CustomerDto customerDto= entityToDto(findOptionalById.get());
+        if(findOptionalById.isPresent()){
+            return customerDto;
+        }
+        */
+
+        // FIND 2.YOL
+        CustomerEntity customerEntity = null;
+        if (id != null) {
+            customerEntity= iCustomerRepository.findById(id).orElseThrow(() -> {
+                throw new HamitMizrakException(id + "bobin yoktur");
+            });
+        }else if(id==null){
+            throw new HamitMizrakException("id is null");
+        }
+        return entityToDto(customerEntity);
     }
 
     // UPDATE
     @Transactional
     @Override
     public CustomerDto customerServiceUpdateFindById(Long id, CustomerDto customerDto) {
-        return null;
+        // Önce Bulmam Lazım
+        CustomerDto customerDtoUpdateFindById=customerServiceFindById(id);
+        if(customerDtoUpdateFindById!=null){
+            CustomerEntity customerEntity = dtoToEntity(customerDtoUpdateFindById);
+            customerEntity.getEmbeddableCustomerEntity().setCustomerName(customerDto.getCustomerName());
+            customerEntity.getEmbeddableCustomerEntity().setCustomerSurname(customerDto.getCustomerSurname());
+            customerEntity.getEmbeddableCustomerEntity().setCustomerNotes(customerDto.getCustomerNotes());
+            customerEntity.getEmbeddableCustomerEntity().setEmail(customerDto.getEmail());
+            customerEntity.getEmbeddableCustomerEntity().setGender(customerDto.getGender());
+            customerEntity.getEmbeddableCustomerEntity().setCustomerTcNumber(customerDto.getCustomerTcNumber());
+            customerEntity.getEmbeddableCustomerEntity().setCustomerVatNumber(customerDto.getCustomerVatNumber());
+            iCustomerRepository.save(customerEntity);
+        }
+        return customerDtoUpdateFindById;
     }
 
     // DELETE
     @Transactional
     @Override
     public CustomerDto customerServiceDeleteFindById(Long id) {
-        return null;
+        // Önce Bulmam Lazım
+        CustomerDto customerDtoDeleteFindById=customerServiceFindById(id);
+        if(customerDtoDeleteFindById!=null){
+            iCustomerRepository.deleteById(id);
+        }
+        return customerDtoDeleteFindById;
     }
 } //end  CustomerServicesImpl

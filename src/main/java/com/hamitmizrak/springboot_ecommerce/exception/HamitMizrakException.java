f9212221e9d910cbb2847e217f0a461bd17f4a8c
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-01 14:29:28 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/springboot_ecommerce/exception/HamitMizrakException.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak.springboot_ecommerce.exception;
+
+// Customise Exception
+public class HamitMizrakException extends RuntimeException {
+
+    // Constructor Parametreli
+    public HamitMizrakException(String message) {
+        super(message);
+    } //end method
+} //end class

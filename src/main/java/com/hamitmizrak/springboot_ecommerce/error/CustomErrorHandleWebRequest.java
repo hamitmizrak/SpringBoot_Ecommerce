e34c0fd91e64098dad7900e5022a9cb288db2978
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-01 15:09:12 +0300

    CustomErrorHandleWebRequest

--- src/main/java/com/hamitmizrak/springboot_ecommerce/error/CustomErrorHandleWebRequest.java
+++ src/main/java/com/hamitmizrak/springboot_ecommerce/error/CustomErrorHandleWebRequest.java
@@ -1,4 +1,90 @@
 package com.hamitmizrak.springboot_ecommerce.error;
 
-public class CustomErrorHandleWebRequest {
-}
+import lombok.RequiredArgsConstructor;
+import com.hamitmizrak.springboot_ecommerce.utils.frontend.ReactFrontend;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// LOMBOK
+@Log4j2 // Loglama
+@RequiredArgsConstructor
+
+// LOMBOK
+// Spring Boot defaulttan gelen error'ı kendimize göre customise yapıyoruz.
+@RestController
+@CrossOrigin(origins = ReactFrontend.PRODUCTION_REACT_FRONTEND_PORT_URL)
+
+// Spring Framework hata mesajları bazen yok yorucu olabilir ancak biz /error ile ilgili hatalı yakalayıp
+// daha sade bir hata mesajı dönderebiliriz.
+public class CustomErrorHandleWebRequest implements ErrorController {
+
+    // Field Injection (1.YOL)
+    /*
+    @Autowired
+    private ErrorAttributes errorAttributes;
+    */
+
+    // Constructor Injection (2.YOL)
+    /*
+    private final ErrorAttributes errorAttributes;
+    // NOT: Eğer 1 tane attributes varsa @Autowired zorunda değilsiz
+    @Autowired
+    public CustomErrorHandleWebRequest(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+    */
+
+    // Lombok Constructor Injection (3.YOL)
+    private final ErrorAttributes errorAttributes;
+
+    // Field
+    private ApiResult apiResult;
+    private String path;
+    private String message;
+    private Integer status;
+    private Map<String, String> validationErrors;
+
+    // http://localhost:4444/error
+    // Spring Frameworkten gelen hataları kendimize göre hataları belirledik (ApiResult)
+    @RequestMapping("/error")
+    public ApiResult handleErrorMethod(WebRequest webRequest){
+        // Spring >=2.3
+        Map<String,Object> attributes=errorAttributes.getErrorAttributes(
+                webRequest,
+                ErrorAttributeOptions
+                        .of(ErrorAttributeOptions.Include.MESSAGE,ErrorAttributeOptions.Include.BINDING_ERRORS)
+        ); //end attributes
+
+        // Spring'ten verileri almak
+        status= (Integer) attributes.get("status");
+        message= (String) attributes.get("message");
+        path= (String) attributes.get("path");
+        // Parametreli Constructor (pms)
+        apiResult= new ApiResult(path,message,status);
+
+        // attributes error varsa
+        if(attributes.containsKey("errors")){
+            List<FieldError> fieldErrorList= (List<FieldError>) attributes.get("errors");
+            validationErrors=new HashMap<>();
+            // for each dongu
+            for(FieldError fieldError : fieldErrorList){
+                validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+            }
+            apiResult.setValidationErrors(validationErrors);
+        }
+        return apiResult;
+    } //end handleErrorMethod
+} // end CustomErrorHandleWebRequest
+
---
+++ src/main/java/com/hamitmizrak/springboot_ecommerce/utils/frontend/ReactFrontend.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.springboot_ecommerce.utils.frontend;
+
+public class ReactFrontend {
+
+    // Const
+    public static final String PRODUCTION_REACT_FRONTEND_PORT_URL="http://localhost:3000";
+    public static final String PRODUCTION_ANGULAR_FRONTEND_PORT_URL="http://localhost:3333";
+}

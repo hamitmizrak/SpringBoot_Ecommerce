4118a73c6c4ce5fcb18101e98d248f5b1ad10e4e
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-01 15:40:45 +0300

    ICustomerServices.java

---
+++ src/main/java/com/hamitmizrak/springboot_ecommerce/business/services/ICustomerServices.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak.springboot_ecommerce.business.services;
+import java.util.List;
+
+
+// D: Dto
+// E: Entity
+public interface ICustomerServices <D,E> {
+
+    // Model Mapper
+    public D entityToDto(E entity);
+    public E dtoToEntity(D dto);
+
+    /////////////////////////////////////////////////////
+    // C R U D
+    // CREATE
+    public D customerServiceCreate(D d);
+
+    // LIST
+    public List<D>  customerServiceList();
+
+    // FIND BY ID
+    public D  customerServiceFindById(Long id);
+
+    // UPDATE
+    public D  customerServiceUpdateFindById(Long id, D d);
+
+    // DELETE
+    public D  customerServiceDeleteFindById(Long id);
+}
